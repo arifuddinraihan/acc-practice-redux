0cb951316ffe7d3e97bf7465bac82bb53d09f65e
arifuddinraihan <mdarifuddinraihan@gmail.com>
2023-02-03 23:20:47 +0600

    added productActions function, added redux devtools

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
 import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/actionCreators/productAction";
 import { ADD_TO_CART } from "../redux/actionTypes/actionTypes";
 
 const ProductCard = ({ product }) => {
@@ -25,7 +26,7 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button onClick={() => dispatch({ type : ADD_TO_CART , payload : product})}
+        <button onClick={() => dispatch(addToCart(product))}
         className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
           Add to cart
         </button>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from "redux"
 import productReducer from "./reducers/productReducer";
+import { composeWithDevTools } from "@redux-devtools/extension"
 
-const newStore = createStore(productReducer);
+const newStore = createStore(productReducer, composeWithDevTools());
 
 export default newStore;
